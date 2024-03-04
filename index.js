e96e8d39f189d767449021c242cac29f00fed74e
huynh <2051010119huynh@ou.edu.vn>
2024-03-04 07:43:24 +0700

    local

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload/images')));
 app.post("/upload", upload.single('imageItem'), (req, res) => {
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://api-amazon-s37l.onrender.com/${req.file.filename}`
     });
 });
 
